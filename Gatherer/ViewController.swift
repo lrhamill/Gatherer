58c2ca99328a29b0d4de8dd6d5934eac4e35b507
Liam Hamill <hamilllr@gmail.com>
2015-06-04 12:13:20 +0100

    Initial Commit

---
+++ Gatherer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Gatherer
+//
+//  Created by Liam Hamill on 04/06/2015.
+//  Copyright (c) 2015 Liam Hamill. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
