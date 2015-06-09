1c4d31fad95de543e727e98ce1cdc813a2a11852
Liam Hamill <hamilllr@gmail.com>
2015-06-09 09:05:14 +0100

    Starting survey task

--- Gatherer/ViewController.swift
+++ Gatherer/ViewController.swift
@@ -7,19 +7,32 @@
 //
 
 import UIKit
+import ResearchKit
+
+extension ViewController : ORKTaskViewControllerDelegate {
+    
+    
+    
+    
+    
+    
+    func taskViewController(taskViewController: ORKTaskViewController, didFinishWithReason reason: ORKTaskViewControllerFinishReason, error: NSError?) {
+        //Handle results with taskViewController.result
+        taskViewController.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+}
 
 class ViewController: UIViewController {
+    
+    
+    
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBAction func consentTapped(sender : AnyObject) {
+        let taskViewController = ORKTaskViewController(task: ConsentTask, taskRunUUID: nil)
+        taskViewController.delegate = self
+        presentViewController(taskViewController, animated: true, completion: nil)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
---
+++ Gatherer/surveyTask.swift
@@ -0,0 +1,9 @@
+//
+//  surveyTask.swift
+//  Gatherer
+//
+//  Created by Liam Hamill on 09/06/2015.
+//  Copyright (c) 2015 Liam Hamill. All rights reserved.
+//
+
+import Foundation
