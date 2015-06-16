661f564913fcec0090674170347cb5d9507e83e4
Liam Hamill <hamilllr@gmail.com>
2015-06-16 13:29:23 +0100

    Cleaned code & added a warning to the withdraw consent button

--- Gatherer/ViewController.swift
+++ Gatherer/ViewController.swift
@@ -25,14 +25,15 @@ extension ViewController : ORKTaskViewControllerDelegate {
         case .Completed:
             
             
-            // If the user has come from the consent form, end without saving.
-            println(consented)
+            // If the user has come from the consent form, update consent info and exit
             if !consented {
                 consented = true
                 taskViewController.dismissViewControllerAnimated(true, completion: nil)
                 return
             }
         
+            // Otherwise, if the user has come from the survey task,
+            // save the results using Core Data
             
             if let resultArray = taskViewController.result.results {
                 
@@ -52,6 +53,7 @@ extension ViewController : ORKTaskViewControllerDelegate {
                 newItem.surveyResult = thisResult
                 
                 println("\(newItem.date): \(newItem.surveyResult)")
+                taskViewController.dismissViewControllerAnimated(true, completion: nil)
                 
             }
 
@@ -63,35 +65,33 @@ extension ViewController : ORKTaskViewControllerDelegate {
             return
             
         }
-            if let resultArray = taskViewController.result.results {
-                
-                var thisResult = 0
-                for item in resultArray {
-                    let results = item.results as! [ORKChoiceQuestionResult]
-                    
-                    if results.count > 0 {
-                        if let answer = results[0].choiceAnswers as? [Int] {
-                            thisResult += answer[0]
-                        }
-                    }
-                }
-                
-                let newItem = NSEntityDescription.insertNewObjectForEntityForName("Result", inManagedObjectContext: self.context!) as! Result
-                newItem.date = NSDate()
-                newItem.surveyResult = thisResult
-                
-                println("\(newItem.date): \(newItem.surveyResult)")
-                
-            }
         
-        taskViewController.dismissViewControllerAnimated(true, completion: nil)
     }
     
 }
 
 class ViewController: UIViewController {
     
-    var consented = false
+    var consented = false {
+        didSet {
+            
+            println("Consented: \(consented)")
+            
+            // Save changes to consented status
+            
+            if let path = NSBundle.mainBundle().pathForResource("ConsentStatus", ofType: "plist") {
+                    if let dict = NSDictionary(contentsOfFile: path) as? Dictionary<String, Bool> {
+                        
+                        var newDict = dict
+                        newDict["Consented"] = consented
+                        
+                        ([newDict] as NSArray).writeToFile(path, atomically: false)
+                    
+                    
+                }
+            }
+        }
+    }
     
     let context = (UIApplication.sharedApplication().delegate as! AppDelegate).managedObjectContext
 
@@ -99,13 +99,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if let path = NSBundle.mainBundle().pathForResource("ConsentStatus", ofType: "plist") {
-            if let dict = NSDictionary(contentsOfFile: path) as? Dictionary<String, AnyObject> {
-                
-                consented = dict["Consented"] as! Bool
-                
-            }
-        }
         
         
     }
@@ -113,8 +106,19 @@ class ViewController: UIViewController {
     override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(animated)
         
+        if let path = NSBundle.mainBundle().pathForResource("ConsentStatus", ofType: "plist") {
+            if let dict = NSDictionary(contentsOfFile: path) as? Dictionary<String, AnyObject> {
+                
+                let plist = dict["Consented"] as! Bool
+                println("plist says: \(plist)")
+                
+                consented = dict["Consented"] as! Bool
+                
+            }
+        }
+        
         if !consented {
-            
+            println(consented)
             launchConsent()
             
         }
@@ -130,6 +134,7 @@ class ViewController: UIViewController {
     
     func launchConsent() {
         
+        println("Launch consent: \(consented)")
         var userConsent = ConsentTask
         let taskViewController = ORKTaskViewController(task: userConsent, taskRunUUID: nil)
         taskViewController.delegate = self
@@ -145,8 +150,26 @@ class ViewController: UIViewController {
     }
 
     @IBAction func withdrawTapped(sender : AnyObject) {
-        consented = false
-        launchConsent()
+        
+        var warning = UIAlertController(title: "Withdrawing consent", message: "Are you sure you want to withdraw from the study?", preferredStyle: UIAlertControllerStyle.Alert)
+        
+        warning.addAction(UIAlertAction(title: "Cancel", style: UIAlertActionStyle.Cancel, handler: nil))
+        
+        
+        warning.addAction(UIAlertAction(title: "Yes", style: .Default, handler: { action in
+            switch action.style{
+            case .Default:
+                self.consented = false
+                self.launchConsent()
+        
+            default:
+                println("cancel")
+
+            }
+        }))
+        
+        self.presentViewController(warning, animated: true, completion: nil)
+
     }
 
 
